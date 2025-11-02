2424de2599b790a974d167d50188b61eda46cc66
Seriously-Warped-Deathmatch <vsbjoi@mail.ru>
2025-11-02 11:20:35 +0300

    Add files via upload

--- Практическая работа 13.1/Director.cs
+++ Практическая работа 13.1/Director.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    internal class Director
+    internal class Director: Employee
     {
+        Employee employee = new Employee { Post = "директор" };
+        public string Department
+        {
+            get { return Department; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    Department = value;
+                }
+                else
+                {
+                    Department = "неизвестно";
+                }
+            }
+        }
+
+        public void budget()
+        {
+            Console.WriteLine($"{Name} утверждает бюджет в отделе {Department}.");
+        }
     }
 }

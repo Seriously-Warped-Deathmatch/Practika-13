07935f5b9839f7ffb08c36cbbd5c55374a6256f9
Seriously-Warped-Deathmatch <vsbjoi@mail.ru>
2025-11-02 18:36:29 +0300

    Add files via upload

--- Практическая работа 13.4/InteractiveCorse.cs
+++ Практическая работа 13.4/InteractiveCorse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    internal class InteractiveCorse
+    internal class InteractiveCorse : Corse
     {
         public int NumberOfExercises
         {
